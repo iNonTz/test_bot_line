a7c710ab14de07d8cda37966f305beef0adbe19a
iNonTz <SmirNoffIRc@gmail.com>
2018-07-25 21:48:15 +0700

    bot1

--- bot.php
+++ bot.php
@@ -14,7 +14,7 @@
     if($message == "สวัสดี"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา !!!!!";
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Sticker"
